093fb5120e77d6a53a65eb7511e4c662709d9086
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-05-24 20:47:04 -0700

    made frontend-backend-igdb test connection

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,50 +3,51 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import { BsPlusCircleFill } from "react-icons/bs";
+import { apiUrl } from "../components/constants";
 
 // Page: Home.
 export const Home = () => {
   // State Object: keeps track of all games in database.
   const [games, setGames] = useState([]);
 
-  // State Object: cookies.
-  const [cookies, setCookies] = useCookies(["sortTerm"]);
+  // // State Object: cookies.
+  // const [cookies, setCookies] = useCookies(["sortTerm"]);
 
-  // useNavigate object
-  const navigate = useNavigate();
+  // // useNavigate object
+  // const navigate = useNavigate();
 
-  // State Objects: keeps track of the navbar terms.
-  const [searchTerm, setSearchTerm] = useState("");
-  const initialSortTerm = cookies.sortTerm || "rating";
-  const [sortTerm, setSortTerm] = useState(initialSortTerm);
+  // // State Objects: keeps track of the navbar terms.
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const initialSortTerm = cookies.sortTerm || "rating";
+  // const [sortTerm, setSortTerm] = useState(initialSortTerm);
 
-  // Functions: sorts games.
-  const handleSortTermChange = (event) => {
-    const newSortTerm = event.target.value;
-    setSortTerm(newSortTerm);
-    setCookies("sortTerm", newSortTerm, { path: "/" });
-  };
-  const sortGames = () => {
-    const sortedGames = games;
-    if (sortTerm === "rating") {
-      return sortedGames.sort((prevGame, nextGame) => {
-        const prevRating = prevGame.rating ?? -Infinity;
-        const nextRating = nextGame.rating ?? -Infinity;
-        return nextRating - prevRating;
-      });
-    } else if (sortTerm === "name") {
-      return sortedGames.sort((prevGame, nextGame) => {
-        return prevGame.name.localeCompare(nextGame.name);
-      });
-    } else if (sortTerm === "releaseDate") {
-      return sortedGames.sort((prevGame, nextGame) => {
-        return new Date(nextGame.releaseDate) - new Date(prevGame.releaseDate);
-      });
-    }
-    return games;
-  };
+  // // Functions: sorts games.
+  // const handleSortTermChange = (event) => {
+  //   const newSortTerm = event.target.value;
+  //   setSortTerm(newSortTerm);
+  //   setCookies("sortTerm", newSortTerm, { path: "/" });
+  // };
+  // const sortGames = () => {
+  //   const sortedGames = games;
+  //   if (sortTerm === "rating") {
+  //     return sortedGames.sort((prevGame, nextGame) => {
+  //       const prevRating = prevGame.rating ?? -Infinity;
+  //       const nextRating = nextGame.rating ?? -Infinity;
+  //       return nextRating - prevRating;
+  //     });
+  //   } else if (sortTerm === "name") {
+  //     return sortedGames.sort((prevGame, nextGame) => {
+  //       return prevGame.name.localeCompare(nextGame.name);
+  //     });
+  //   } else if (sortTerm === "releaseDate") {
+  //     return sortedGames.sort((prevGame, nextGame) => {
+  //       return new Date(nextGame.releaseDate) - new Date(prevGame.releaseDate);
+  //     });
+  //   }
+  //   return games;
+  // };
 
-  sortGames();
+  // sortGames();
 
   // On Render Function: fetches all games' data from database.
   useEffect(() => {
@@ -54,7 +55,7 @@ export const Home = () => {
     const fetchGame = async () => {
       try {
         const response = await axios.get(
-          "https://ludi-server.vercel.app/games"
+          `${apiUrl}/games`
         );
         setGames(response.data);
       } catch (err) {
@@ -66,15 +67,33 @@ export const Home = () => {
     fetchGame();
   }, []);
 
+  // TODO: Finish function for retrieving infro from IGDB database
+  const refreshList = () => {
+    const refreshList = async () => {
+      try {
+        const response = await axios.post(`${apiUrl}/games/igdb/games`, {
+          query: "fields *; where id = 127;",
+        });
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    refreshList();
+  };
+
   return (
     <div className="home">
       <div className="page-bar">
         <Link to="/add-game" className="page-bar-btn">
           <BsPlusCircleFill style={{ marginRight: "8px" }} /> Add a game
         </Link>
+        <button className="page-bar-btn" onClick={refreshList}>
+          Refresh list
+        </button>
       </div>
 
-      <ul className="title-list">
+      {/* <ul className="title-list">
         {games
           .filter((game) => {
             if (searchTerm === "") {
@@ -98,7 +117,7 @@ export const Home = () => {
               </Link>
             </li>
           ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
