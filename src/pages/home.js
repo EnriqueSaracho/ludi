071dce2766dc981d809b18353e9c4f9daf55dcd3
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-07-01 13:20:03 -0700

    Working on fetching data for homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsPlusCircleFill } from "react-icons/bs";
 import { apiUrl } from "../components/constants";
 
@@ -9,77 +8,52 @@ import { apiUrl } from "../components/constants";
 export const Home = () => {
   // State Object: keeps track of all games in database.
   const [games, setGames] = useState([]);
-
-  // // State Object: cookies.
-  // const [cookies, setCookies] = useCookies(["sortTerm"]);
-
-  // // useNavigate object
-  // const navigate = useNavigate();
-
-  // // State Objects: keeps track of the navbar terms.
-  // const [searchTerm, setSearchTerm] = useState("");
-  // const initialSortTerm = cookies.sortTerm || "rating";
-  // const [sortTerm, setSortTerm] = useState(initialSortTerm);
-
-  // // Functions: sorts games.
-  // const handleSortTermChange = (event) => {
-  //   const newSortTerm = event.target.value;
-  //   setSortTerm(newSortTerm);
-  //   setCookies("sortTerm", newSortTerm, { path: "/" });
-  // };
-  // const sortGames = () => {
-  //   const sortedGames = games;
-  //   if (sortTerm === "rating") {
-  //     return sortedGames.sort((prevGame, nextGame) => {
-  //       const prevRating = prevGame.rating ?? -Infinity;
-  //       const nextRating = nextGame.rating ?? -Infinity;
-  //       return nextRating - prevRating;
-  //     });
-  //   } else if (sortTerm === "name") {
-  //     return sortedGames.sort((prevGame, nextGame) => {
-  //       return prevGame.name.localeCompare(nextGame.name);
-  //     });
-  //   } else if (sortTerm === "releaseDate") {
-  //     return sortedGames.sort((prevGame, nextGame) => {
-  //       return new Date(nextGame.releaseDate) - new Date(prevGame.releaseDate);
-  //     });
-  //   }
-  //   return games;
-  // };
+  // const [imageId, setImageId] = useState("");
 
   // sortGames();
 
   // On Render Function: fetches all games' data from database.
   useEffect(() => {
-    // Function: fetches all games' data from database
-    const fetchGame = async () => {
-      try {
-        const response = await axios.get(
-          `${apiUrl}/games`
-        );
-        setGames(response.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    // Can't have an "on render" async function, so we call the async function inside the "on render" one.
-    fetchGame();
+    fetchInfo();
   }, []);
 
-  // TODO: Finish function for retrieving infro from IGDB database
-  const refreshList = () => {
-    const refreshList = async () => {
+  // Function for retrieving info from IGDB database
+  const fetchInfo = async () => {
+    const fetchGames = async () => {
       try {
-        const response = await axios.post(`${apiUrl}/games/igdb/games`, {
-          query: "fields *; where id = 127;",
+        const response = await axios.post(`${apiUrl}/igdb/games`, {
+          query: "fields name, cover; limit 15; where cover != null;",
         });
-        console.log(response.data);
+
+        let coverIds = response.data;
+        return coverIds;
       } catch (err) {
         console.log(err);
       }
     };
-    refreshList();
+
+    const fetchCovers = async (coverIds) => {
+      for (let obj of coverIds) {
+        try {
+          const response = await axios.post(`${apiUrl}/igdb/covers`, {
+            query: `fields image_id; where id = ${obj.cover};`,
+          });
+
+          obj.image_id = response.data[0].image_id;
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      return coverIds;
+    };
+
+    const coverIds = await fetchGames();
+    if (coverIds) {
+      const response = await fetchCovers(coverIds);
+      setGames(response);
+      console.log(games);
+    }
   };
 
   return (
@@ -88,11 +62,34 @@ export const Home = () => {
         <Link to="/add-game" className="page-bar-btn">
           <BsPlusCircleFill style={{ marginRight: "8px" }} /> Add a game
         </Link>
-        <button className="page-bar-btn" onClick={refreshList}>
-          Refresh list
-        </button>
+        {/* <button className="page-bar-btn" onClick={fetchInfo}>
+          Refresh
+        </button> */}
       </div>
 
+      {/* <img
+        src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${imageId}.jpg`}
+        alt="ac2 cover"
+      /> */}
+
+      {/* TODO: Make li elements hold a Link element to a page dedicated to each game */}
+      <ul className="title-list">
+        {games.map((game) => (
+          <li key={game._id} className="thumbnail">
+            <Link to={`/game/${game._id}`} className="thumbnail-link">
+              {game[0].image_id ? (
+                <img
+                  src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${game[0].image_id}.jpg`}
+                  alt={game[0].name}
+                  className="thumbnail-img"
+                />
+              ) : (
+                <p>Loading image...</p>
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
       {/* <ul className="title-list">
         {games
           .filter((game) => {
