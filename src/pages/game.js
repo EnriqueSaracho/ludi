31fb2a46388efe964c8746ede935ab1430da05ea
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-08-29 18:53:17 -0700

    Testing request number limits

--- src/pages/game.js
+++ src/pages/game.js
@@ -71,7 +71,7 @@ export const Game = () => {
       await fetchNameAndDate(gameData.about.version_parent); // Fetching 'name' and 'first_release_date' for 'version_parent'
       await fetchInvolvedCompanyInfo(gameData.about.involved_companies); // Fetching 'company', 'developer', 'porting', 'publisher', and 'supporting' for 'involved_companies'
       await fetchNames(gameData.about.genres, "genres"); // Fetching 'name' for 'genres'
-      await sleep(1000);
+      // await sleep(1000);
       await fetchNames(gameData.about.themes, "themes"); // Fetching 'name' for 'themes'
       await fetchNames(gameData.about.game_modes, "game_modes"); // Fetching 'name' for 'game_modes'
       await fetchNames(
@@ -79,7 +79,7 @@ export const Game = () => {
         "player_perspectives"
       ); // Fetching 'name' for 'player_perspectives'
       await fetchNames(gameData.about.collections, "collections"); // Fetching 'name' for 'collections'
-      await sleep(1000);
+      // await sleep(1000);
       await fetchNames(gameData.about.franchises, "franchises"); // Fetching 'name' for 'franchises'
       await fetchNames(gameData.about.game_engines, "game_engines"); // Fetching 'name' for 'game_engines'
       await fetchNamesAndAbbreviations(gameData.about.platforms, "platforms"); // Fetching 'name' and 'abbreviation' for 'platforms'
